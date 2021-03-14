9fdb93b99260fae26e0705e571684ecf5bebe11f
Jamal <jamensky1991@gmail.com>
2021-03-14 23:32:25 +0000

    test

---
+++ db/migrate/20210314222646_create_figure_titles.rb
@@ -0,0 +1,13 @@
+class CreateFigureTitles < ActiveRecord::Migration
+  def change
+    create_table :figures_titles do |t|
+      t.integer :figure_id 
+      t.integer :title_id 
+   end 
+  end
+end
+
+
+
+
+
